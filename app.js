d2de55c161969558fa3c84a4fea94513be59ceed
pgregoretti <pgregoretti@gmail.com>
2023-01-02 22:57:41 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('foobar'));
+app.get('/', (req, res) => res.send("badabing badaboom"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
