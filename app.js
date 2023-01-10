4b91304fa4bbad8eba5736122f6c89803280cd6d
pgregoretti <prg5rv@virginia.edu>
2023-01-10 17:09:04 -0500

    uploading website again 2

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const path = require('path')
 
-app.get('/', (req, res) => res.send("badabing badaboom"));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'))
+  //res.sendFile(path.join(__dirname, 'views/test.html'))
+});
+
+//app.get('/', (req, res) => res.send("badabing badaboom"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
